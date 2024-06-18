96acae9a2b8a4e4a5a85c471c0cf38f9e0ce02db
Nuno Cunha <nuno.cunha@technipfmc.com>
2024-06-18 14:03:53 -0300

    subnet added

--- main.tf
+++ main.tf
@@ -242,10 +242,10 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = ["10.0.0.0/16"]
   #dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
-  # subnet {
-  #   name           = "subnet1"
-  #   address_prefix = "10.0.1.0/24"
-  # }
+  subnet {
+    name           = "Default"
+    address_prefix = "10.0.1.0/24"
+  }
 
   # subnet {
   #   name           = "subnet2"
